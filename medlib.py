1597729c21416a4499e33b55213ef5be4819f194
Animesh <animeshcode07.com>
2025-03-25 17:13:45 +0530

    To know how format string work

---
+++ medlib.py
@@ -0,0 +1,12 @@
+#project 1
+
+adj=input("Adjective:")
+verb1=input("Verb 1:")
+verb2=input("Verb 2:")
+famous_person=input("Famous Person:")
+
+medlib=f"Computer programming is so {adj}! It make me so exited all the time because \
+    I love to {verb1}. Stay Hyderated and enjoy the day. \
+    My Famous person is {famous_person}"
+
+print(medlib)
