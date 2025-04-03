e1dad983d4fd225fc526eb5a6de92d3d958946e3
Animesh <animeshcode07.com>
2025-04-03 15:16:47 +0530

    how __name__ works

---
+++ area.py
@@ -0,0 +1,7 @@
+import rectangle
+def calculate_area(a,b):
+    return a*b
+
+if __name__=="__main__":
+    print("The area of square is :",calculate_area(3,3))
+    print("The area of rectangle is:",rectangle.calculate_area_rectangle())
---
+++ rectangle.py
@@ -0,0 +1,4 @@
+import area
+
+def calculate_area_rectangle():
+    return area.calculate_area(2, 3)
